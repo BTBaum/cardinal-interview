0c11cea29fad5efd2b3d01afc385cdef0f289c25
Brandon <railsdev14@gmail.com>
2016-01-05 10:54:06 -0500

    Added interview server routes

---
+++ modules/interviews/server/routes/interviews.server.routes.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Module dependencies
+var interviewsPolicy = require('../policies/interviews/interviews.server.policy'),
+    interviews = require('../controllers/interviews.server.controller');
+
+module.exports = function (app) {
+  //Interviews collection routes
+  app.route('./api/interviews').all(interviewsPolicy.isAllowed)
+    .get(interviews.list)
+    .post(interviews.create);
+
+  // Single Interview routes
+  app.route('./api/interviews/:interviewId').all(interviewsPolicy.isAllowed)
+    .get(interviews.read)
+    .put(interviews.update)
+    .delete(interviews.delete);
+
+  // Bind the interview to middleware
+  app.param('interviewId', interviews.interviewByID);
+};
