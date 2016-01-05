6c3fc36b3a44e0e289e975614569e25e48035d78
Brandon <railsdev14@gmail.com>
2016-01-05 10:46:16 -0500

    Add interviews server policy/permissions for api

---
+++ modules/interviews/server/policies/interviews.server.policy.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//Module dependencies
+var acl = require('acl');
+
+// Using the memory backend
+acl = new acl(new acl.memoryBackend());
+
+// Invoke Interviews Permissions
+exports.invokeRolesPolicies = function () {
+  acl.allow([{
+    roles: ['admin'],
+    allows: [{
+      resources: '/api/interviews',
+      permissions: '*'
+    }]
+  },
+  {
+    roles: ['user'],
+    allows: [{
+      resources: '/api/interviews',
+      permissions: ['get', 'post']
+    },
+    {
+      resources: '/api/interviews/:interviewId',
+      permissions: ['get']
+    }]
+  },
+  {
+    roles: ['guest'],
+    allows: [{
+      resources: '/api/interviews',
+      permissions: ['get']
+    },
+    {
+      resources: '/api/interviews/:interviewId',
+      permissions: ['get']
+    }]
+  }]);
+};
